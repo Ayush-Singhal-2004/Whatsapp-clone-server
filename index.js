2b410d1d91a10812a23adb9fe049fb2be6647a8e
Ayush-Singhal-2004 <ayushsinghal1904@gmail.com>
2024-02-24 14:03:48 +0530

    updated package.json

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config'
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.use(cors());    
 
